f45736c71e530f027c52137a4bac34a8b1095cb9
asimd2472 <asimd2472@gmail.com>
2026-06-08 18:57:28 +0530

    error fix

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::fallback(function () {
 
 
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/',[AuthController::class,'index'])->name('login');
+    Route::get('/login',[AuthController::class,'index'])->name('login');
     Route::post('login', [AuthController::class, 'login'])->name('login.submit');
 
     Route::middleware(['auth','is_admin'])->group(function () {
